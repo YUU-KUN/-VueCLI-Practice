49cda91c0a49707081b5d3e43f440f18084e3328
YUU-KUN <indra_wahyu_27rpl@student.smktelkom-mlg.sch.id>
2020-09-01 12:29:11 +0700

    'done'

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <First msg="First Component"></First>
+  </div>
+</template>
+
+<script>
+import First from './components/First.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+    First
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/First.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'First',
+    props: {
+        msg: String,
+    }
+}
+</script>
+
+<style scoped>
+    h1{
+        color: white;
+        padding: 10px 50px;
+        background-color: #2abd7a;
+    }
+</style>
